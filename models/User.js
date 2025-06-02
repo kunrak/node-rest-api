e96b615db6cfef05fb066703211cc142e6cc5097
kunrak <rakesh.kundu@csgi.com>
2025-06-02 18:42:42 +0530

    Creating User Model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { profile } = require("console");
+const mongoose = require("mongoose");
+const { type } = require("os");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      min: 3,
+      max: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      max: 50,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-  res.send("hey its auth route");
+router.get("/register", async (req, res) => {
+  const user = await new User({
+    username: "john",
+    email: "john@gmail.com",
+    password: "123456789",
+  });
+
+  await user.save();
+  res.send("OKKKKKKKKKKKKK");
 });
 
 module.exports = router;
